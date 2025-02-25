508535db8595e9388fd54dfb7116e9015012f573
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-02-25 12:58:13 +0100

    troquei nome e fiz teste no console
    - nome para nao confundir
    - teste para ver se estava pegando correto

---
+++ script.js
@@ -0,0 +1,9 @@
+
+console.log("iniciando script.js")
+
+const todo = document.getElementById("todoInput")
+
+const addTodo = document.getElementById("addTodoButton")
+
+console.log(todo)
+console.log(addTodo)
