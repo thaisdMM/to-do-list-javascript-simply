0308bbb0212de4a07ead71df6abdb4f5c05a48a5
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-02-26 20:00:16 +0100

    string vazia

--- script.js
+++ script.js
@@ -2,10 +2,18 @@ const todo = document.getElementById("todoInput"); // Pega o input
 const addTodo = document.getElementById("addTodoButton"); // Pega o botão
 const listTodo = document.getElementById("todoList"); // Pega a <ul>
 
+  /*
+	.	todoValue.trim()
+	•	todoValue é o valor que o usuário digitou no input.
+	•	O método .trim() remove espaços extras antes e depois do texto.
+	•	Se o usuário só digitou espaços, trim() transforma isso em uma string vazia ""*/
+
 function addTodoTask() {
   const todoValue = todo.value; // Pega o valor do input 
-   console.log(todoValue);
-   
+  if (todoValue.trim() === "") {
+   console.log("Campo vazio, digite algo primeiro");
+   return
+  }
    const newTodoItem = document.createElement("li"); // Cria um <li>
    newTodoItem.textContent = todoValue; // Define o texto do <li> como o que foi digitado
  
