d7864a9181a57ebfa4b961d11d012ac2e793ed78
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-02-27 14:06:38 +0100

    created li and add to ul

--- todo_second_code/script.js
+++ todo_second_code/script.js
@@ -4,7 +4,14 @@ let inputTask = document.getElementById("inputTodo");
 
 let addButton = document.getElementById("addTodo");
 
+let listTask = document.getElementById("todoList")
+
 function addTodoTask() {
   let valueInputTask = inputTask.value;
   console.log(valueInputTask)
+
+  let newTaskItem = document.createElement("li")
+  newTaskItem.textContent = valueInputTask
+  listTask.appendChild(newTaskItem)
+
 }
