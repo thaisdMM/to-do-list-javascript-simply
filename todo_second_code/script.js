b3222a4057c341dd96a860b9b976cea8e99992d1
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-02-27 13:45:53 +0100

    button working

---
+++ todo_second_code/script.js
@@ -0,0 +1,9 @@
+console.log("script funcionando")
+
+let inputTask = document.getElementById("inputTodo")
+
+let addButton = document.getElementById("addTodo")
+
+function addTodoTask() {
+   console.log("clicou no botão")
+}
