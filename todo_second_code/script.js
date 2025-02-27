e71371dd643ebb8307cf8f27d6434b8194427735
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-02-27 14:20:42 +0100

    add delete button and make working

--- todo_second_code/script.js
+++ todo_second_code/script.js
@@ -4,14 +4,24 @@ let inputTask = document.getElementById("inputTodo");
 
 let addButton = document.getElementById("addTodo");
 
-let listTask = document.getElementById("todoList")
+let listTask = document.getElementById("todoList");
 
 function addTodoTask() {
   let valueInputTask = inputTask.value;
-  console.log(valueInputTask)
+  console.log(valueInputTask);
 
-  let newTaskItem = document.createElement("li")
-  newTaskItem.textContent = valueInputTask
-  listTask.appendChild(newTaskItem)
+  let newTaskItem = document.createElement("li");
+  newTaskItem.textContent = valueInputTask;
 
+  let deleteTask = document.createElement("button");
+  deleteTask.textContent = "Delete";
+  deleteTask.type = "button";
+
+  newTaskItem.appendChild(deleteTask);
+
+  listTask.appendChild(newTaskItem);
+
+  deleteTask.addEventListener("click", function () {
+    newTaskItem.remove();
+  });
 }
