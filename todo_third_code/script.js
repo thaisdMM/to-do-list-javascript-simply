8ab52cc8f1e5b3dec1677e9f7d3713f464b17869
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-02-28 11:40:04 +0100

    created UL list

--- todo_third_code/script.js
+++ todo_third_code/script.js
@@ -1,25 +1,32 @@
-const htmlContent = document.getElementById("htmlContent")
+const htmlContent = document.getElementById("htmlContent");
 
 function createInput() {
-   const inputTodo = document.createElement("input")
-    inputTodo.type = "text"
-    inputTodo.id = "inputTodo"
-    inputTodo.placeholder = "type your to-do task"
+  const inputTodo = document.createElement("input");
+  inputTodo.type = "text";
+  inputTodo.id = "inputTodo";
+  inputTodo.placeholder = "type your to-do task";
 
-   htmlContent.appendChild(inputTodo)
+  htmlContent.appendChild(inputTodo);
 }
-createInput()
+createInput();
+
+function createUlList() {
+  const ulList = document.createElement("ul");
+  ulList.id = "ulList";
+
+  htmlContent.appendChild(ulList);
+}
 
 function createAddButton() {
-   const addButton = document.createElement("button")
-   addButton.textContent = "add to-do"
-   addButton.type = "button"
-   addButton.id = "addButton"
+  const addButton = document.createElement("button");
+  addButton.textContent = "add to-do";
+  addButton.type = "button";
+  addButton.id = "addButton";
 
-   addButton.addEventListener("click", () =>
-      console.log("cliquei no botão")
-   )
+  addButton.addEventListener("click", () => {
+    console.log("clicou");
+  });
 
-   htmlContent.appendChild(addButton)
+  htmlContent.appendChild(addButton);
 }
-createAddButton()
+createAddButton();
