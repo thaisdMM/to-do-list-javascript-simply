d97685549c68d6a2ee9f639592cb08fd2a17ef45
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-02-28 10:40:50 +0100

    created input and addButton

---
+++ todo_third_code/script.js
@@ -0,0 +1,21 @@
+const htmlContent = document.getElementById("htmlContent")
+
+function createInput() {
+   const inputTodo = document.createElement("input")
+    inputTodo.type = "text"
+    inputTodo.id = "inputTodo"
+    inputTodo.placeholder = "type your to-do task"
+
+   htmlContent.appendChild(inputTodo)
+}
+createInput()
+
+function createAddButton() {
+   const addButton = document.createElement("button")
+   addButton.textContent = "add to-do"
+   addButton.type = "button"
+   addButton.id = "addButton"
+
+   htmlContent.appendChild(addButton)
+}
+createAddButton()
